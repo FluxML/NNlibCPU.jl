9ac94c5ef1d563d2fced40f43de2e38d1909e89a
chriselrod <elrodc@gmail.com>
2021-06-18 18:21:46 -0400

    Started adding pooling layers

--- src/NNlibCPU.jl
+++ src/NNlibCPU.jl
@@ -1,11 +1,17 @@
 module NNlibCPU
 
 using NNlib, LoopVectorization, Polyester, StrideArrays, ChainRulesCore, ForwardDiff
+using CloseOpenIntervals
 using StrideArrays: AbstractStrideArray
-# using Octavian
+using Static: Zero, One
+
+using NNlib: input_size, output_size, kernel_size, channels_out, padding, dilation,
+  calc_padding_regions
+
 
 # `dense.jl` is waiting on Octavian support
 include("dense.jl")
 include("conv.jl")
+include("maxpool.jl")
 
 end
